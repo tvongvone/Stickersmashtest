4c666fd88a98d4abf7615bcd8880cd6be24a9e57
Tony Vongvone <tvongvone@gmail.com>
2024-09-10 22:46:45 -0700

    Emoji picker update

--- components/EmojiPicker.js
+++ components/EmojiPicker.js
@@ -20,3 +20,29 @@ export default function EmojiPicker({ isVisible, children, onClose }) {
     </Modal>
   );
 }
+
+const styles = StyleSheet.create({
+  modalContent: {
+    height: '25%',
+    width: '100%',
+    backgroundColor: '#25292e',
+    borderTopRightRadius: 18,
+    borderTopLeftRadius: 18,
+    position: 'absolute',
+    bottom: 0,
+  },
+  titleContainer: {
+    height: '16%',
+    backgroundColor: '#464C55',
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
