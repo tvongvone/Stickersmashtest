8529405dfe884909c62578f96eb78b665369a0d4
Tony Vongvone <tvongvone@gmail.com>
2024-10-01 22:36:43 -0700

    updated EmojiSticker

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,6 +1,23 @@
 import { View, Image } from 'react-native';
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+  const doubleTap = Gesture.Tap()
+  .numberOfTaps(2)
+  .onStart(() => {
+    if (scaleImage.value !== imageSize * 2) {
+      scaleImage.value = scaleImage.value * 2;
+    }
+  });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
+
+
   return (
     <View style={{ top: -350 }}>
       <Image
