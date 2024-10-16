e1f18164c941ade081d3a84b6481f00045ca1e04
Tony Vongvone <tvongvone@gmail.com>
2024-10-15 21:52:45 -0700

    EmojiSticker.js

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,13 +1,24 @@
-import { View, Image } from 'react-native';
+import { View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
-export default function EmojiSticker({ imageSize, stickerSource }) {
+type Props = {
+  imageSize: number;
+  stickerSource: string;
+};
+
+export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
   const doubleTap = Gesture.Tap()
-  .numberOfTaps(2)
-  .onStart(() => {
-    if (scaleImage.value !== imageSize * 2) {
-      scaleImage.value = scaleImage.value * 2;
-    }
-  });
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      }
+    });
 
   const imageStyle = useAnimatedStyle(() => {
     return {
@@ -34,15 +45,17 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
     };
   });
 
-
-
   return (
-    <View style={{ top: -350 }}>
-      <Image
-        source={stickerSource}
-        resizeMode="contain"
-        style={{ width: imageSize, height: imageSize }}
-      />
-    </View>
+    /* @tutinfo Wrap all components inside GestureDetector. */<GestureDetector gesture={drag}>
+      <Animated.View style={/* @tutinfo Add containerStyle to the AnimatedView style prop. */[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </GestureDetector>
+      </Animated.View>
+    /* @tutinfo */</GestureDetector>
   );
 }
