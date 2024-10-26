7e039f65693f9b82bd65e7bf2f73b0a96dc943cb
Tony Vongvone <tvongvone@gmail.com>
2024-10-25 22:10:54 -0700

    tsx

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -2,10 +2,10 @@ import { View } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
-// type Props = {
-//   imageSize: number;
-//   stickerSource: string;
-// };
+type Props = {
+  imageSize: number;
+  stickerSource: string;
+};
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   const scaleImage = useSharedValue(imageSize);
