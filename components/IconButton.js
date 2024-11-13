d73142be6339a5c09c8abbe64c83e48b8a3ab367
Tony Vongvone <tvongvone@gmail.com>
2024-11-12 19:40:12 -0800

    enable IconButton

--- components/IconButton.js
+++ components/IconButton.js
@@ -2,9 +2,9 @@ import {View, Pressable} from 'react-native'
 
 export default function IconButton() {
     return (
-        // <Pressable style={styles.iconButton} onPress={onPress}>
-        //     <MaterialIcons name={icon} size={24} color="#fff" />
-        //     <Text style={styles.iconButtonLabel}>{label}</Text>
-        // </Pressable>
+        <Pressable style={styles.iconButton} onPress={onPress}>
+            <MaterialIcons name={icon} size={24} color="#fff" />
+            <Text style={styles.iconButtonLabel}>{label}</Text>
+        </Pressable>
     )
 }
