112b5a63fa0fbc95be11a2a246f65d8c0e835163
Tony Vongvone <tvongvone@gmail.com>
2024-11-19 21:28:32 -0800

    change image view

--- components/EmojiPicker.js
+++ components/EmojiPicker.js
@@ -7,7 +7,7 @@ import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function EmojiPicker({ isVisible, children, onClose }) {
   return (
-    // <Modal animationType="slide" transparent={true} visible={isVisible}>
+    <Modal animationType="slide" transparent={true} visible={isVisible}>
     //   <View style={styles.modalContent}>
     //     <View style={styles.titleContainer}>
     //       <Text style={styles.title}>Choose a sticker</Text>
--- components/ImageView.js
+++ components/ImageView.js
@@ -1,23 +1,21 @@
-import { StyleSheet, Image } from "react-native";
+import { StyleSheet } from 'react-native';
+import { Image, type ImageSource } from "expo-image";
 
-// Expected Format for Image Source in React Native: In React Native, when you want to display
-// an image, you typically use the Image component or similar components like ImageBackground.
-// These components expect the source prop to be an object with a uri property pointing to the image's location.
+type Props = {
+  imgSource: ImageSource;
+  selectedImage?: string;
+};
 
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
 
-export default function ImageView({placeHolderImage, selectedImage}) {
-    const imageSource = selectedImage ? {uri: selectedImage} : placeHolderImage
-
-    return (
-        <Image source={imageSource} style={styles.image}/>
-    )
+  return <Image source={imageSource} style={styles.image} />;
 }
 
-
 const styles = StyleSheet.create({
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18
-    }
-})
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
