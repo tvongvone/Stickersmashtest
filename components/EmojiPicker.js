cf5427d2a5ddea5d18559309c1a82df6a2f7e17f
Tony Vongvone <tvongvone@gmail.com>
2024-11-29 17:50:12 -0800

    Emoji picker

--- components/EmojiPicker.js
+++ components/EmojiPicker.js
@@ -7,17 +7,17 @@ import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function EmojiPicker({ isVisible, children, onClose }) {
   return (
-    <Modal animationType="slide" transparent={true} visible={isVisible}>
-    //   <View style={styles.modalContent}>
-    //     <View style={styles.titleContainer}>
-    //       <Text style={styles.title}>Choose a sticker</Text>
-    //       <Pressable onPress={onClose}>
-    //         <MaterialIcons name="close" color="#fff" size={22} />
-    //       </Pressable>
-    //     </View>
-    //     {children}
-    //   </View>
-    // </Modal>
+    // <Modal animationType="slide" transparent={true} visible={isVisible}>
+    // //   <View style={styles.modalContent}>
+    // //     <View style={styles.titleContainer}>
+    // //       <Text style={styles.title}>Choose a sticker</Text>
+    // //       <Pressable onPress={onClose}>
+    // //         <MaterialIcons name="close" color="#fff" size={22} />
+    // //       </Pressable>
+    // //     </View>
+    // //     {children}
+    // //   </View>
+    // // </Modal>
   );
 }
 
