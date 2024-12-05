be8c9ff276963fd74ea302f851a5af960e76c906
Tony Vongvone <tvongvone@gmail.com>
2024-12-04 20:55:00 -0800

    button styling bug

--- components/Button.js
+++ components/Button.js
@@ -23,31 +23,31 @@ export default function Button(props) {
     )
 }
 
-const styles = StyleSheet.create({
-    buttonContainer: {
-        width: 320,
-        height: 68,
-        marginHorizontal: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 3
-    },
+// const styles = StyleSheet.create({
+//     buttonContainer: {
+//         width: 320,
+//         height: 68,
+//         marginHorizontal: 20,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         padding: 3
+//     },
 
-    button: {
-        borderRadius: 10,
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row'
-    },
+//     button: {
+//         borderRadius: 10,
+//         width: '100%',
+//         height: '100%',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         flexDirection: 'row'
+//     },
 
-    buttonIcon: {
-        paddingRight: 8
-    },
+//     buttonIcon: {
+//         paddingRight: 8
+//     },
 
-    buttonLabel: {
-        color: '#fff',
-        fontSize: 16
-    }
-})
+//     buttonLabel: {
+//         color: '#fff',
+//         fontSize: 16
+//     }
+// })
