a87edddfe59875c4b43264ec32833e6fde4006f3
Tony Vongvone <tvongvone@gmail.com>
2024-12-13 20:08:23 -0800

    --

--- components/Button.js
+++ components/Button.js
@@ -15,39 +15,39 @@ export default function Button(props) {
     }
 
     return (
-        <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={props.onPress}>
-                <Text style={styles.label}>{props.label}</Text>
-            </Pressable>
-        </View>
+        // <View style={styles.buttonContainer}>
+        //     <Pressable style={styles.button} onPress={props.onPress}>
+        //         <Text style={styles.label}>{props.label}</Text>
+        //     </Pressable>
+        // </View>
     )
 }
 
-const styles = StyleSheet.create({
-    buttonContainer: {
-        width: 320,
-        height: 68,
-        marginHorizontal: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 3
-    },
+// const styles = StyleSheet.create({
+//     buttonContainer: {
+//         width: 320,
+//         height: 68,
+//         marginHorizontal: 20,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         padding: 3
+//     },
 
-    button: {
-        borderRadius: 10,
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row'
-    },
+//     button: {
+//         borderRadius: 10,
+//         width: '100%',
+//         height: '100%',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         flexDirection: 'row'
+//     },
 
-    buttonIcon: {
-        paddingRight: 8
-    },
+//     buttonIcon: {
+//         paddingRight: 8
+//     },
 
-    buttonLabel: {
-        color: '#fff',
-        fontSize: 16
-    }
-})
+//     buttonLabel: {
+//         color: '#fff',
+//         fontSize: 16
+//     }
+// })
